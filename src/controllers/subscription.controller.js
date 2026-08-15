569c03eb4eca98903af3e0bc821ba933e21ef91b
Mudasir <apple.mudasir@gmail.com>
2026-08-15 12:15:55 +0700

    feat: add getSubscribedChannels endpoint to fetch channel subscribers with details

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -78,7 +78,47 @@ const getUserChannelSubscribers = asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200, subscribers, "Channel subscribers fetched successfully"));
 })
 
+const getSubscribedChannels = asyncHandler(async(req,res)=>{
+
+  const channelId = req.params.channelId
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(201,"Please enetr valid object id");
+  }
+  console.log("channel",channelId);
+  const subscribers = await Subscription.aggregate([
+    {
+      $match:{
+        channel:new mongoose.Types.ObjectId(channelId)
+      }
+    },{
+      $lookup:{
+        from:"users",
+        localField:"channel",
+        foreignField:"_id",
+        as: "channelDetails"
+      }
+    },  // same like previous only change the channel and as to channel details then channelDetails.id
+    {
+       $unwind: "$channelDetails"
+   },
+   {
+    $project:{
+      _id: "$channelDetails._id",
+      username: "$channelDetails.username",
+      avatar: "$channelDetails.avatar",
+      createdAt:1
+    }
+   }
+
+  ])
+  res
+    .status(200)
+    .json(new ApiResponse(200, subscribers, "Channel subscribers fetched successfully"));
+})
+
+
 export {
     toggleSubscription,  //perfect
-    getUserChannelSubscribers //working perfect
+    getUserChannelSubscribers, //working perfect
+    getSubscribedChannels ////working perfect
 }
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express"
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 import { Subscription } from "../models/subscription.models.js";
-import { toggleSubscription , getUserChannelSubscribers } from "../controllers/subscription.controller.js";
+import { toggleSubscription , getUserChannelSubscribers , getSubscribedChannels } from "../controllers/subscription.controller.js";
 
 const router =Router();
 
 router.route("/subscribers/:channelId").post(verfiyJWT, toggleSubscription);
 router.route("/getUserChannel/:channelId").post(verfiyJWT, getUserChannelSubscribers);
+router.route("/getSubscribed/:channelId").post(verfiyJWT, getSubscribedChannels);
+
+
 
 export default router;
