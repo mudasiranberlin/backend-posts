cf14e63350d0c7fa5de8aaf9b9bd894450a30184
Mudasir <apple.mudasir@gmail.com>
2026-08-17 11:06:49 +0700

    finsihed the dashboard : feat: enhance getChannelStats and getChannelVideos functions to include likes count and improve response structure

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -30,14 +30,36 @@ const getChannelStats = asyncHandler(async (req, res) => {
         }
     ])
 
+    console.log(views);
+
+    const totalViews = views[0]?.totalViews || 0;
+
+    console.log("Total views", totalViews);
+
+
     const subscribe = await Subscription.countDocuments({
         channel: userid
     })
 
-    const totalViews = views[0]?.totalViews || 0;
+
 
     const userVideos = await Video.find({ owner: userid }).select("_id");
+
+    // you can use the for of loop to get the ids or map loop
+    /*
+        const videosids = []
+        for (const element of userVideos) {
+            videosids.push(element._id)
+        }
+        console.log("videosids",videosids);
+    
+        */
+
     const videoIds = userVideos.map((v) => v._id);
+
+    console.log("videoIds", videoIds);
+
+
     const totalLikes = await Like.countDocuments({ video: { $in: videoIds } });
 
     return res
@@ -54,16 +76,45 @@ const getChannelStats = asyncHandler(async (req, res) => {
                 "Comment has been deleted "
             )
         );
-
-
-
-
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const userId = req.user._id;
 
+    const videos = await Video.find({ owner: userId }).populate("owner", "_id username avatar").sort({ createdAt: -1 }).lean()
+
+    const videosWithLikes = [];
+    const currentUserId = req.user?._id;
+    for (const video of videos) {
+        const likesCount = await Like.countDocuments({
+            video: video._id
+        });
+
+        const isLiked = currentUserId
+            ? !!(await Like.findOne({
+                video: video._id,
+                likeBy: currentUserId
+            }))
+            : false;
+
+        videosWithLikes.push({
+            ...video,
+            likesCount,
+            isLiked
+        });
+    }
     console.log("reached");
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                201,
+                { videosWithLikes },
+                "Channel videos fetched successfully"
+            )
+        );
 
 })
 
